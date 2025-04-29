c65bcbd5310eed0e21d5d256d2f73aa12081eea3
Christian iervasi <c.iervasi@studenti.unisa.it>
2025-04-29 12:46:30 +0200

    initial commit

---
+++ essentia-api/src/test/java/com/essentia/essentia_api/EssentiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essentia.essentia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentiaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
