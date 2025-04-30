8828cbe7a710cf612135e55747f219079ae7e292
Christian iervasi <c.iervasi@studenti.unisa.it>
2025-04-30 22:54:37 +0200

    .

--- essentia-api/src/main/java/com/essentia/essentiaAdministration/entity/PerfumePrfNotes.java
+++ essentia-api/src/main/java/com/essentia/essentiaAdministration/entity/PerfumePrfNotes.java
@@ -1,8 +1,11 @@
 package com.essentia.essentiaAdministration.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "perfume_prfnotes")
 public class PerfumePrfNotes {
     
 	@ManyToOne
