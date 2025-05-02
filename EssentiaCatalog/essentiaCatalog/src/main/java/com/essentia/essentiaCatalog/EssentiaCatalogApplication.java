0e0cd7748eb85fbd6e0f4c181092044e63e1d4f4
Christian iervasi <c.iervasi@studenti.unisa.it>
2025-05-02 22:28:30 +0200

    Essentia Catalog API

---
+++ EssentiaCatalog/essentiaCatalog/src/main/java/com/essentia/essentiaCatalog/EssentiaCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.essentia.essentiaCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentiaCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentiaCatalogApplication.class, args);
+	}
+
+}
---
+++ EssentiaCatalog/essentiaCatalog/src/test/java/com/essentia/essentiaCatalog/EssentiaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essentia.essentiaCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssentiaCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
