267145d372b0626b062041278912bd874a87b791
CHRISTIAN IERVASI <ciervasi@emeal.nttdata.com>
2025-05-02 18:57:09 +0200

    Validation Error handling

--- essentia-api/src/main/java/com/essentia/essentiaAdministration/exception/GlobalExceptionHandler.java
+++ essentia-api/src/main/java/com/essentia/essentiaAdministration/exception/GlobalExceptionHandler.java
@@ -1,5 +1,39 @@
 package com.essentia.essentiaAdministration.exception;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ResponseBody
+	public ErrorResponse handleValidationErrors (MethodArgumentNotValidException e) {
+		
+		ErrorResponse response = new ErrorResponse();
+		List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+		List<Violation>violations = new ArrayList<>();
+
+		for(FieldError fieldError : fieldErrors) {
+			Violation violation = new Violation(fieldError.getField(), fieldError.getDefaultMessage());
+			violations.add(violation);
+		}
+		response.setViolations(violations);
+		response.setTimestamp(LocalDateTime.now());
+		response.setError("Errore di validazione");
+		response.setMessage("Uno o più campi non rispettano i criteri di validazione");
+		return response;
+		
+	}
+
 }
