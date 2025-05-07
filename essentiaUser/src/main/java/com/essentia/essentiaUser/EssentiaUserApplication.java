cdf508011b9a40d684967f4f930c5ceb3d86d21c
CHRISTIAN IERVASI <ciervasi@emeal.nttdata.com>
2025-05-07 10:26:25 +0200

    essentia user

---
+++ essentiaUser/src/main/java/com/essentia/essentiaUser/EssentiaUserApplication.java
@@ -0,0 +1,13 @@
+package com.essentia.essentiaUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentiaUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssentiaUserApplication.class, args);
+	}
+
+}
