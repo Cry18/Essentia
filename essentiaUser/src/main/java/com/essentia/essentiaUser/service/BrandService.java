8ae9eeebeddcc9d79cfe9ae928de3c2fb0386e67
CHRISTIAN IERVASI <ciervasi@emeal.nttdata.com>
2025-05-07 11:28:47 +0200

    Update BrandService.java

--- essentiaUser/src/main/java/com/essentia/essentiaUser/service/BrandService.java
+++ essentiaUser/src/main/java/com/essentia/essentiaUser/service/BrandService.java
@@ -2,7 +2,6 @@ package com.essentia.essentiaUser.service;
 
 import java.util.List;
 
-
 public interface BrandService {
     List<String> findAllBrands();
     List<String> findLikeNameBrands(String name);
