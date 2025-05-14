e578789f4b1c204e7cdc7c8f0443938c24a5252c
CHRISTIAN IERVASI <ciervasi@emeal.nttdata.com>
2025-05-14 11:55:20 +0200

    log dep and test
    added log4j2 dependencies and first log test

--- essentiaUser/src/main/java/com/essentia/essentiaUser/controller/SignatureController.java
+++ essentiaUser/src/main/java/com/essentia/essentiaUser/controller/SignatureController.java
@@ -1,6 +1,9 @@
 package com.essentia.essentiauser.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +16,8 @@ import com.essentia.essentiauser.service.impl.UserServiceImpl;
 @RequestMapping("/api/user/")
 public class SignatureController {
     
+    private static final Logger logger = LogManager.getLogger(SignatureController.class);
+
     @Autowired
     private UserServiceImpl userServiceImpl;
 
@@ -20,5 +25,10 @@ public class SignatureController {
     public String setSignaure(@RequestParam(value = "userId") int userId, 
                             @RequestParam(value = "perfumeId") int perfumeId) {
         return userServiceImpl.setSignature(userId, perfumeId);
+    }
+
+    @GetMapping("log")
+    public void logTesting(){
+        logger.debug("prima prova di log");
     }
 }
