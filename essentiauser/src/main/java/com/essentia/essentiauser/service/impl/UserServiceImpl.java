71545b0eab09ab3a933788f504977d82c8a656ea
Christian iervasi <c.iervasi@studenti.unisa.it>
2025-05-14 16:47:13 +0200

    log

--- essentiauser/src/main/java/com/essentia/essentiauser/service/impl/UserServiceImpl.java
+++ essentiauser/src/main/java/com/essentia/essentiauser/service/impl/UserServiceImpl.java
@@ -35,22 +35,22 @@ public class UserServiceImpl implements UserService {
         logger.debug("Fetching user with id: {}", userId);
             User user = userRepository.findById(userId);
                 if (user == null) {
-                    logger.debug("User not found with id: {}", userId);
+                    logger.warn("User not found with id: {}", userId);
                     throw new ResourceNotFoundException("User not found");
                 }
             logger.debug("Fetching perfume with id: {}", perfumeId);
             Perfume perfume = perfumeRepository.findById(perfumeId);
                 if (perfume == null) {
-                    logger.debug("Perfume not found with id: {}", perfumeId);
+                    logger.warn("Perfume not found with id: {}", perfumeId);
                     throw new ResourceNotFoundException("Perfume not found");
                 }
             if (user.getFavorites().contains(perfume)) {
-                logger.debug("Perfume with id: {} already in favorites", perfumeId);
+                logger.warn("Perfume with id: {} already in favorites", perfumeId);
                 throw new ResourceNotFoundException("Perfume already in favorites");
             }
             user.addFavorite(perfume);
             userRepository.save(user);
-            logger.debug("Perfume with id: {} added to favorites", perfumeId);
+            logger.info("Perfume with id: {} added to favorites", perfumeId);
             return perfume.getName() + " added to favorites";
         }
 
@@ -59,65 +59,66 @@ public class UserServiceImpl implements UserService {
         logger.debug("Fetching user with id: {}", userId);
         User user = userRepository.findById(userId);
                 if (user == null) {
-                    logger.debug("User not found with id: {}", userId);
+                    logger.warn("User not found with id: {}", userId);
                     throw new ResourceNotFoundException("User not found");
                 }
             logger.debug("Fetching perfume with id: {}", perfumeId);
         Perfume perfume = perfumeRepository.findById(perfumeId);
                 if (perfume == null) {
-                    logger.debug("Perfume not found with id: {}", perfumeId);
+                    logger.warn("Perfume not found with id: {}", perfumeId);
                     throw new ResourceNotFoundException("Perfume not found");
                 }
 
         if (user.removeFavorite(perfume)) {
             userRepository.save(user);
-            logger.debug("Perfume with id: {} removed from favorites", perfumeId);
+            logger.info("Perfume with id: {} removed from favorites", perfumeId);
             return perfume.getName() + " removed from favorites";
         } else {
-            logger.debug("Perfume with id: {} not found in favorites for user with id: {}", perfumeId, user);
+            logger.warn("Perfume with id: {} not found in favorites for user with id: {}", perfumeId, user);
             throw new ResourceNotFoundException("There is no perfume with id: " + perfumeId + " in user's favorites with id: " + userId);}
     }
+
     @Override
     public String setSignature(int userId, int perfumeId){
         logger.debug("Fetching user with id: {}", userId);
         User user = userRepository.findById(userId);
         if (user == null) {
-            logger.debug("User not found with id: {}", userId);
+            logger.warn("User not found with id: {}", userId);
             throw new ResourceNotFoundException("User not found");
         }	
         logger.debug("Fetching perfume with id: {}", perfumeId);
         Perfume perfume = perfumeRepository.findById(perfumeId);
         if (perfume == null) {
-            logger.debug("Perfume not found with id: {}", perfumeId);
+            logger.warn("Perfume not found with id: {}", perfumeId);
             throw new ResourceNotFoundException("Perfume not found");
         }
         user.setSignature(perfume);
 		userRepository.save(user);
-        logger.debug("Perfume with id: {} set as signature for user with id: {}", perfumeId, userId);
+        logger.info("Perfume with id: {} set as signature for user with id: {}", perfumeId, userId);
         return perfume.getName() + " set as signature for user with id : " + userId;
     }
 
     @Override
     public ReviewDto createReview(int userId, int perfumeId, ReviewDto review){
-        if (reviewRepository.findByUserId(userId) != null) {
-            logger.debug("User with id: {} already has a review for perfume with id: {}", userId, perfumeId);
+        if (reviewRepository.findByUserIdAndPerfumeId(userId, perfumeId) != null) {
+            logger.warn("User with id: {} already has a review for perfume with id: {}", userId, perfumeId);
             throw new DuplicateReviewException("User already has a review for this perfume");
         }
         logger.debug("Fetching user with id: {}", userId);
         User user = userRepository.findById(userId);
         if (user == null) {
-            logger.debug("User not found with id: {}", userId);
+            logger.warn("User not found with id: {}", userId);
             throw new ResourceNotFoundException("User not found");
         }
         logger.debug("Fetching perfume with id: {}", perfumeId);
         Perfume perfume = perfumeRepository.findById(perfumeId);
         if (perfume == null) {
-            logger.debug("Perfume not found with id: {}", perfumeId);
+            logger.warn("Perfume not found with id: {}", perfumeId);
             throw new ResourceNotFoundException("Perfume not found");
         }
         Review reviewEntity = new Review(perfume, user, review.getTitle(), review.getDescription(), review.getVote(), review.getSeasonality(), review.isGender(), review.getSillage(), review.getLongevity());
         reviewRepository.save(reviewEntity);
-        logger.debug("Review created for perfume with id: {} by user with id: {}", perfumeId, userId);
+        logger.info("Review created for perfume with id: {} by user with id: {}", perfumeId, userId);
         review.setId(reviewEntity.getId());
         return review;
     }
@@ -128,14 +129,14 @@ public class UserServiceImpl implements UserService {
         Review review = reviewRepository.findById(reviewId);
         if (review != null) {
             if (review.getUser().getId() != userId) {
-                logger.debug("User with id: {} does not own the review with id: {}", userId, reviewId);
+                logger.warn("User with id: {} does not own the review with id: {}", userId, reviewId);
                 throw new ForbiddenActionException("This review does not belong to the user with id: " + userId);
             } else {
-            logger.debug("Deleting review with id: {}", reviewId);
             reviewRepository.delete(review);
+            logger.info("Review with id: {}, deleted", reviewId);
             return "Review with id: " + reviewId + " deleted";}
         } else {
-            logger.debug("Review not found with id: {} for user with id: {}", reviewId, userId);
+            logger.warn("Review not found with id: {} for user with id: {}", reviewId, userId);
             throw new ResourceNotFoundException("There is no review with id: " + reviewId + " for user with id: " + userId);}
     }
 
